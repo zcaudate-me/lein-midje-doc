7ae567216855882a01485f4690221e76692c7164
Chris Zheng <z@caudate.me>
2013-08-14 23:38:31 +1000

    First commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject lein-midje-doc "0.1.0-SNAPSHOT"
+  :description "Documentation generator for midje"
+  :url "http://www.github.com/zcaudate/lein-midje-doc"
+  :license {:name "The MIT License"
+            :url "http://http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [instaparse "1.2.2"]]
+  :eval-in-leiningen true)
---
+++ src/leiningen/midje_doc.clj
@@ -0,0 +1,6 @@
+(ns leiningen.midje-doc)
+
+(defn midje-doc
+  "I don't do a lot."
+  [project & args]
+  (println "Hi!"))
