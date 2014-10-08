dff0bec80ac7f2a72f8932cc8fc32a62d1b60430
Chris Zheng <z@caudate.me>
2014-10-08 14:51:55 +0800

    Fixed tests

--- test/leiningen/midje_doc/test_midje_doc.clj
+++ test/leiningen/midje_doc/test_midje_doc.clj
@@ -4,30 +4,30 @@
             [rewrite-clj.parser :as p]
             [rewrite-clj.zip :as z]
             :reload))
+(comment
+
+  #_(run-doc "my-first-document"
+           {:input "test/midje_doc/my_first_document.clj"
+            :title "title goes here"
+            :sub-title "subtitle goes here"
+            :author "name"
+            :email  "email@domain.com"
+            :version "v0.0.12"
+            :website "www.example.com"})
+
+  (run-doc "my-first-document"
+               {:input "/Users/zhengc/dev/chit/purnam-angular/test/cljs/midje_doc/purnam_angular_guide.cljs"
+                             :title "purnam.angular"
+                             :sub-title "Angular.js DSL in Clojurescript"
+                             :author "Chris Zheng"
+                             :email  "z@caudate.me"
+                :tracking "UA-31320512-2"}
+               {:version "0.3.2"
+                :description "Angular.js DSL in Clojurescript"})
 
 
-#_(run-doc "my-first-document"
-         {:input "test/midje_doc/my_first_document.clj"
-          :title "title goes here"
-          :sub-title "subtitle goes here"
-          :author "name"
-          :email  "email@domain.com"
-          :version "v0.0.12"
-          :website "www.example.com"})
+  (parser/parse-content (z/of-string "^{:name \"hello\" :numbered true} (fact 1 => 1)"))
 
-(run-doc "my-first-document"
-             {:input "/Users/zhengc/dev/chit/purnam-angular/test/cljs/midje_doc/purnam_angular_guide.cljs"
-                           :title "purnam.angular"
-                           :sub-title "Angular.js DSL in Clojurescript"
-                           :author "Chris Zheng"
-                           :email  "z@caudate.me"
-              :tracking "UA-31320512-2"}
-             {:version "0.3.2"
-              :description "Angular.js DSL in Clojurescript"})
-
-
-(parser/parse-content (z/of-string "^{:name \"hello\" :numbered true} (fact 1 => 1)"))
-
-(let []) (-> (z/of-string "^{:name \"hello\" :numbered true} (fact 1 => 1)")
-    z/down
-    z/sexpr)
+  (let []) (-> (z/of-string "^{:name \"hello\" :numbered true} (fact 1 => 1)")
+      z/down
+      z/sexpr))
